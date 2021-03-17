1c7ffc9708f6546c32f776660962b4bd482fdc42
haya-cod <80845234+haya-cod@users.noreply.github.com>
2021-03-17 23:50:42 +0700

    Teh pan

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2020',
-  name: 'Darlene'
+  birthdate: '18 maret, 2021',
+  name: 'Pani Nurjanah'
 };
 
 function hideEverything() {
